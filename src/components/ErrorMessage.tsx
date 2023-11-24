ab3185c1c4a1858ae61ec24ae8b0d766e14a658e
Lalbek Sherbekov <sherbekovlalbek21@gmail.com>
2023-11-24 20:43:46 +0100

    react-type-23

---
+++ src/components/ErrorMessage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+interface ErrorMessageProps {
+error : string
+}
+
+export function ErrorMessage({error}: ErrorMessageProps) {
+  return (
+    <p className="text-center text-red-700">{error}</p>
+    )
+}
+
+
---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export function AboutPage() {
+  return (
+    <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+         Consectetur nesciunt aliquid saepe harum sequi, quaerat aspernatur, provident odit, at illum tenetur! Suscipit tenetur porro odit ipsam optio temporibus laudantium at.
+ </div>
+  )
+}
+
+
